4476effaecf7f438a1a8f6f25928b461cdfb2352
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-19 13:07:48 -0700

    queue tests passing

--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,20 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
+    #use an array!
 
   def enqueue(self, item):
-    pass
+    #add an item to the back of the queue
+    self.size += 1
+    self.storage.append(item)
   
   def dequeue(self):
-    pass
+    #remove and return item from front of queue
+    #check if queue is populated
+    if len(self.storage) is not 0:
+      self.size -= 1
+      return self.storage.pop(0)
 
   def len(self):
-    pass
+    return len(self.storage)
