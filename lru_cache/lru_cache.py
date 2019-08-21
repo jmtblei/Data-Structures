e6b2af3e35c3ac8d821536f60fdf4cf3a579fa9d
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-21 13:38:19 -0700

    lru cache pass

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,7 +1,15 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
   def __init__(self, limit=10):
-    pass
-
+    #needs to store key value pairs (dict)
+    #needs to track the order
+    #needs to store cache
+    #needs to store the size of cache
+    self.limit = limit
+    self.size = 0
+    self.storage = dict()
+    self.order = DoublyLinkedList
   """
   Retrieves the value associated with the given key. Also
   needs to move the key-value pair to the top of the order
@@ -10,7 +18,12 @@ class LRUCache:
   key-value pair doesn't exist in the cache. 
   """
   def get(self, key):
-    pass
+    if key in self.storage:
+      node = self.storage[key]
+      self.order.move_to_front(node)
+      return node.value[1]
+    else:
+      return None
 
   """
   Adds the given key-value pair to the cache. The newly-
@@ -23,4 +36,18 @@ class LRUCache:
   the newly-specified value. 
   """
   def set(self, key, value):
-    pass
+    #update value and move to head if key already in cache
+    if key in self.storage:
+      node = self.storage(key)
+      node.value = (key, value)
+      self.order.move_to_front(node)
+      return
+    #remove the oldest (tail) if the value is at max
+    if self.size == self.limit:
+      del self.storage[self.order.tail.value[0]]
+      self.order.remove_from_tail()
+      self.size -= 1
+    #add to head if pair to cache makes it the most recent
+    self.order.add_to_head((key, value))
+    self. storage[key] = self.order.head
+    self.size += 1
