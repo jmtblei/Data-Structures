b3167e0da7af0de0e8186d65f5ec23a2b3f3a1f0
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-08-22 15:13:02 -0700

    insert heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,9 +3,19 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    # Check if storage is empty, if it is start at beginning
+    # Put the value at the end of the list (append)
+    # if Arr[i] > Arr[i/2] then swap (parent is bigger)
+    self.storage.append(value)
+    current_index = self.get_size() - 1
+    self._bubble_up(current_index)
+    # print(len(self.storage))
 
   def delete(self):
+    # delete(): Deleting a key also takes O(Logn) time. We replace the 
+    # key to be deleted with minum infinite by calling decreaseKey(). 
+    # After decreaseKey(), the minus infinite value must reach root, 
+    # so we call extractMin() to remove the key.
     pass
 
   def get_max(self):
